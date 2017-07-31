a16886863e1bde07260a4b8b922683b1abf0cc69
cc <15355498770@163.com>
2017-08-01 07:52:51 +0800

    07.31 initial

--- src/main/java/mysql/UserRepository.java
+++ src/main/java/mysql/UserRepository.java
@@ -1,6 +1,7 @@
 package mysql;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Repository;
  * 可以不加 @Repository("userRepository")
  * spring1.5 会自动扫描产生bean
  */
+@RepositoryRestResource(collectionResourceRel = "user", path = "user")
 public interface UserRepository extends CrudRepository<User, Integer> {
 
 }
